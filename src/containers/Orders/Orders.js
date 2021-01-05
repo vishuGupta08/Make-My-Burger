3c3bf960f5a096a0ea2dba31b839a80a83e12678
Vishu Gupta <vishu.gupta.dce@gmail.com>
2021-01-05 19:07:35 +0530

    added Your Orders and some editing in build control

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -5,6 +5,7 @@ class Orders extends Component {
     render() {
         return (
             <div>
+                <div>  <h2 style={{ textAlign: 'center', border: '2px solid lightblue', boxSizing: 'border-box' }}>Your Orders</h2> </div>
                 <Order />
                 <Order />
             </div>
