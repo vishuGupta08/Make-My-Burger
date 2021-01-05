b0fa24788668a42b81a6fa0e29832cf0b0275f6d
Vishu Gupta <vishu.gupta.dce@gmail.com>
2021-01-05 12:35:49 +0530

    added placed order and redirected to home page

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -87,30 +87,12 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        //     this.setState({ loading: true })
-        //     const order = {
-        //         ingredients: this.state.ingredients,
-        //         price: this.state.totalPrice,
-        //         customer: {
-        //             name: 'VIshu Gupta',
-        //             address: 'B-180 Sai Baba APartment',
-        //             zipcode: '110085',
-        //             country: 'India',
-        //             contact: '8920673899',
-        //             email: 'vishu.gupta.dtu@gmail.com'
-        //         }
-        //     }
-        //     axios.post('/orders.json', order)
-        //         .then(response => {
-        //             this.setState({ loading: false, purchasing: false })
-        //         })
-        //         .catch(error => {
-        //             this.setState({ loading: false, purchasing: false })
-        //         })
+
         const queryParams = [];
         for (let i in this.state.ingredients) {
             queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]))
         }
+        queryParams.push('price=' + this.state.totalPrice)
         const queryString = queryParams.join('&')
         this.props.history.push({
             pathname: '/checkout',
--- src/containers/ContactData/ContactData.js
+++ src/containers/ContactData/ContactData.js
@@ -2,6 +2,11 @@
 import Button from '../../components/UI/Button/Button'
 import React, { Component } from 'react'
 import classes from './ContactData.css'
+import axios from '../../axios-orders'
+import Spinner from '../../components/UI/Spinner/Spinner'
+import Modal from '../../components/UI/Modal/Modal'
+import Aux from '../../hoc/auxi'
+
 class ContactData extends Component {
 
     state = {
@@ -13,21 +18,55 @@ class ContactData extends Component {
         }
     }
 
+    orderHandler = (event) => {
+        event.preventDefault();
+        this.setState({ loading: true })
+        const order = {
+            ingredients: this.props.ingredients,
+            price: this.props.price,
+            customer: {
+                name: 'VIshu Gupta',
+                address: 'B-180 Sai Baba APartment',
+                zipcode: '110085',
+                country: 'India',
+                contact: '8920673899',
+                email: 'vishu.gupta.dtu@gmail.com'
+            },
+            loading: false
+        }
+        axios.post('/orders.json', order)
+            .then(response => {
+                this.setState({ loading: false })
+                this.props.history.push('/')
+            })
+            .catch(error => {
+                this.setState({ loading: false })
+            })
+
+    }
+
     render() {
+
+        let form = (<form className={classes.Form} >
+            <input className={classes.Input} type="text" name="name" placeholder="Your Name" id="name"></input>
+            <input className={classes.Input} type="email" name="email" placeholder="Your Email Address" id="email"></input>
+            <input className={classes.Input} type="text" name="flatNumber" placeholder="Your Flat Number" id="add"></input>
+            <input className={classes.Input} type="text" name="zipcode" placeholder="Zip Code"></input>
+            <div> <Button btnType="Success" clicked={this.orderHandler} >Order</Button></div>
+        </form>);
+        if (this.state.loading) {
+            form = (
+                <Aux>
+                    <Spinner />
+                    <Modal show> Order Placed Successfully!</Modal>
+                </Aux>
+            )
+        }
         return (
             <div className={classes.ContactData}>
-                <h4>
-                    Enter Your Contact Details
-</h4>
+                <h4>Enter Your Contact Details</h4>
                 <div className={classes.FormAlign}>
-                    <form className={classes.Form} >
-
-                        <input className={classes.Input} type="text" name="name" placeholder="Your Name" id="name"></input>
-                        <input className={classes.Input} type="email" name="email" placeholder="Your Email Address" id="email"></input>
-                        <input className={classes.Input} type="text" name="flatNumber" placeholder="Your Flat Number" id="add"></input>
-                        <input className={classes.Input} type="text" name="zipcode" placeholder="Zip Code"></input>
-                        <div> <Button btnType="Success" >Order</Button></div>
-                    </form>
+                    {form}
                 </div>
             </div>
 
