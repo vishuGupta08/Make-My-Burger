68bd1b82371e6cf9ddef392e06f816e804ffa992
Vishu Gupta <vishu.gupta.dce@gmail.com>
2021-01-14 20:13:49 +0530

    corrected the issue in environment variable

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -7,7 +7,7 @@ const navigationItems = (props) => {
     return (
         <ul className={classes.NavigationItems}>
             <NavigationItem link="/" exact >Burger Builder </NavigationItem>
-            {props.isAuthenticated ? <NavigationItem link="/orders" > Orders </NavigationItem> : null}
+            {props.isAuthenticated ? <NavigationItem link="/orders" > Your Orders </NavigationItem> : null}
             {props.isAuthenticated ? <NavigationItem link="/logout" >Logout </NavigationItem> : <NavigationItem link="/auth" >Login </NavigationItem>}
         </ul >
     )
--- src/index.js
+++ src/index.js
@@ -12,7 +12,13 @@ import thunk from 'redux-thunk'
 import authReducer from './store/reducers/auth'
 
 
-const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose
+let composeEnhancers = null;
+if (process.env.NODE_ENV === 'development') {
+    composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+} else {
+    composeEnhancers = compose;
+}
+
 const rootReducer = combineReducers({
     burgerBuilder: burgerBuilderReducer,
     order: orderReducer,
